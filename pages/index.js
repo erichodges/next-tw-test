c0068f77873e427655ef0c6574130f0cd5ed640b
Eric Hodges <ehodges2004@gmail.com>
2021-07-25 15:54:49 -0700

    Add: next.config.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,33 @@
-import Head from 'next/head'
+// import Head from 'next/head'
+
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div>hey yo</div>
-  )
+    <div
+      style={{
+        position: 'relative',
+        height: 0,
+        zIndex: -10
+      }}
+    >
+      <div
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          zIndex: 10
+        }}
+      >
+        <Image
+          src='https://res.cloudinary.com/dn3baky3u/image/upload/v1580246844/gatsby-cloudinary/evora-gt-side.jpg'
+          alt='car'
+          width='2400'
+          height='1200'
+          layout='responsive'
+          priority='true'
+        />
+      </div>
+    </div>
+  );
 }
